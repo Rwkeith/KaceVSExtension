ea33dfd6bd61dc69e5be4144ebed55edb967e1c7
Rwkeith <lookup@nothing.in>
2023-12-05 18:46:02 -0800

    first commit

---
+++ CleanTablesFinal/CleanTable.cs
@@ -0,0 +1,125 @@
+﻿using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using System;
+using System.Diagnostics;
+using System.ComponentModel.Design;
+using System.Globalization;
+using System.Threading;
+using System.Threading.Tasks;
+using Task = System.Threading.Tasks.Task;
+
+namespace CleanTable
+{
+    /// <summary>
+    /// Command handler
+    /// </summary>
+    internal sealed class CleanTable
+    {
+        /// <summary>
+        /// Command ID.
+        /// </summary>
+        public const int CommandId = 0x0100;
+
+        /// <summary>
+        /// Command menu group (command set GUID).
+        /// </summary>
+        public static readonly Guid CommandSet = new Guid("7717d54c-29f5-4a18-b88a-2fd7b8e2e465");
+
+        /// <summary>
+        /// VS Package that provides this command, not null.
+        /// </summary>
+        private readonly AsyncPackage package;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CleanTable"/> class.
+        /// Adds our command handlers for menu (commands must exist in the command table file)
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        /// <param name="commandService">Command service to add command to, not null.</param>
+        private CleanTable(AsyncPackage package, OleMenuCommandService commandService)
+        {
+            this.package = package ?? throw new ArgumentNullException(nameof(package));
+            commandService = commandService ?? throw new ArgumentNullException(nameof(commandService));
+
+            var menuCommandID = new CommandID(CommandSet, CommandId);
+            var menuItem = new MenuCommand(this.Execute, menuCommandID);
+            commandService.AddCommand(menuItem);
+        }
+
+        /// <summary>
+        /// Gets the instance of the command.
+        /// </summary>
+        public static CleanTable Instance
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Gets the service provider from the owner package.
+        /// </summary>
+        private Microsoft.VisualStudio.Shell.IAsyncServiceProvider ServiceProvider
+        {
+            get
+            {
+                return this.package;
+            }
+        }
+
+        /// <summary>
+        /// Initializes the singleton instance of the command.
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        public static async Task InitializeAsync(AsyncPackage package)
+        {
+            // Switch to the main thread - the call to AddCommand in CleanTable's constructor requires
+            // the UI thread.
+            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync(package.DisposalToken);
+
+            OleMenuCommandService commandService = await package.GetServiceAsync(typeof(IMenuCommandService)) as OleMenuCommandService;
+            Instance = new CleanTable(package, commandService);
+        }
+
+        /// <summary>
+        /// This function is the callback used to execute the command when the menu item is clicked.
+        /// See the constructor to see how the menu item is associated with this function using
+        /// OleMenuCommandService service and MenuCommand class.
+        /// </summary>
+        /// <param name="sender">Event sender.</param>
+        /// <param name="e">Event args.</param>
+        private void Execute(object sender, EventArgs e)
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+
+            // string solutionPath = Environment.GetEnvironmentVariable("SolutionDir");
+            EnvDTE.DTE dte = (EnvDTE.DTE)System.Runtime.InteropServices.Marshal.GetActiveObject("VisualStudio.DTE");
+
+            string solutionPath = System.IO.Path.GetDirectoryName(dte.Solution.FullName);
+
+            solutionPath += "\\x64\\Debug\\CleanTable.exe";
+            Debug.WriteLine($"Solution.FullName: {solutionPath}");
+
+            try
+            {
+                // Start the process
+                var startInfo = new ProcessStartInfo
+                {
+                    FileName = solutionPath, // Replace with the path to your program
+                    UseShellExecute = true,
+                    Verb = "runas" // This verb triggers run as administrator
+                };
+
+                var process = System.Diagnostics.Process.Start(startInfo);
+                process.WaitForExit(); // Wait for the process to complete
+
+                Debug.WriteLine($"Stopping the Debugger...");
+                dte.ExecuteCommand("Debug.StopDebugging");
+            }
+            catch (Exception ex)
+            {
+                // Handle exceptions (like user cancelling UAC prompt)
+                Debug.WriteLine($"Error starting process: {ex.Message}");
+            }
+        }
+    }
+}
